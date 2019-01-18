23989b90ae050628ef977e2aedaecfd07b93d528
salaz <salaz@DESKTOP-IVOPF5P>
2019-01-18 19:46:50 +0100

    Aula

---
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/Aula.java
@@ -0,0 +1,55 @@
+package org.iesalandalus.programacion.reservasaulas;
+
+public class Aula {
+	private String nombre;
+
+	public Aula(String nombre) {
+		if (nombre == null) {
+			throw new IllegalArgumentException("El nombre del aula no puede ser nulo.");
+		}
+		if (nombre == "") {
+			throw new IllegalArgumentException("El nombre del aula no puede estar vacío.");
+		}
+		setNombre(nombre);
+	}
+	public Aula(Aula aula) {
+		if (aula == null) {
+			throw new IllegalArgumentException("No se puede copiar un aula nula.");
+		}
+		setNombre(aula.nombre);
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	private void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aula other = (Aula) obj;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "[nombre=" + nombre + "]";
+	}
+	
+}
--- src/test/java/org/iesalandalus/programacion/reservasaulas/modelo/dominio/AulaTest.java
+++ src/test/java/org/iesalandalus/programacion/reservasaulas/modelo/dominio/AulaTest.java
@@ -1,7 +1,15 @@
 package org.iesalandalus.programacion.reservasaulas.modelo.dominio;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
+
+import org.iesalandalus.programacion.reservasaulas.Aula;
+import org.junit.Test;
+
 public class AulaTest {
-	/*
+	
 	private static final String ERROR_EXCEPCION = "Debería haber saltado la excepción.";
 	private static final String ERROR_NO_EXCEPCION = "No debería haber saltado la excepción.";
 	private static final String nombre = "Salón de actos";
@@ -95,5 +103,5 @@ public class AulaTest {
 		Aula aula = new Aula(nombre);
 		assertEquals("[nombre=Salón de actos]", aula.toString());
 	}
-*/
+
 }
