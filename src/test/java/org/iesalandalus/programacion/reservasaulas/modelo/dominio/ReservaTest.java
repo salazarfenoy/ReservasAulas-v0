11ce711f74d1af4a1f9def4e412afcc0f2da42a1
salaz <salaz@DESKTOP-IVOPF5P>
2019-01-19 12:56:53 +0100

    Reserva

--- src/test/java/org/iesalandalus/programacion/reservasaulas/modelo/dominio/ReservaTest.java
+++ src/test/java/org/iesalandalus/programacion/reservasaulas/modelo/dominio/ReservaTest.java
@@ -1,7 +1,21 @@
 package org.iesalandalus.programacion.reservasaulas.modelo.dominio;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
+
+import java.time.LocalDate;
+
+import org.iesalandalus.programacion.reservasaulas.Aula;
+import org.iesalandalus.programacion.reservasaulas.Permanencia;
+import org.iesalandalus.programacion.reservasaulas.Profesor;
+import org.iesalandalus.programacion.reservasaulas.Reserva;
+import org.iesalandalus.programacion.reservasaulas.Tramo;
+import org.junit.Test;
+
 public class ReservaTest {
-	/*
+	
 	private static final String ERROR_EXCEPCION = "Debería haber saltado la excepción.";
 	private static final String ERROR_NO_EXCEPCION = "No debería haber saltado la excepción.";
 	private static final String nombreProfesor = "José Ramón";
@@ -115,5 +129,5 @@ public class ReservaTest {
 				+ ", aula=[nombre=Salón de actos], permanencia=[dia=01/12/2018, tramo=Mañana]]";
 		assertEquals(cadenaEsperada, reserva.toString());
 	}
-*/
+
 }
